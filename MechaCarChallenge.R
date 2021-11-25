25d6cd4e7079348c5a33a2b16d93be3c07e5437a
Teruki Ito <ito.teruki@gmail.com>
2021-11-24 22:16:46 -0800

    summarized suspension coil weight capacity data

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,8 +1,21 @@
 #import dependency
 library(dplyr)
 
-#import and read MechaCar_mpg.csv into tibble
+#import and read MechaCar_mpg.csv
 mecha_mpg<-read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
 #perform linear regression on data, get summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mecha_mpg))
+
+#import and read suspension_coil.csv
+suspension_coil<- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#summary of suspension coil PSI data
+total_summary <- suspension_coil %>% summarize (Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+print(total_summary)
+
+#summary of suspension coil PSI data by manufacturing lot
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize (Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+print(lot_summary)
